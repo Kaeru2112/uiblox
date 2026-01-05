fd2e3784eea7bc63cfdd7894f58e947a2fccaa47
Kaeru2112 <118968112+Kaeru2112@users.noreply.github.com>
2026-01-06 00:01:19 +0700

    Update UI text and window name in main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,21 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Kaeru Hub", HidePremium = false, SaveConfig = true})
+local Window = OrionLib:MakeWindow({Name = "Kaeru Hub | Blox Fruits", HidePremium = false, SaveConfig = true, ConfigFolder = "KaeruConfig"})
 
 local Tab = Window:MakeTab({
-    Name = "Main",
+    Name = "Chính",
     Icon = "rbxassetid://4483345998",
     PremiumOnly = false
 })
 
 Tab:AddButton({
-    Name = "Test UI",
+    Name = "Kiểm tra Script",
     Callback = function()
-              OrionLib:MakeNotification({Name = "Thành công!", Content = "UI của bạn đã hoạt động", Image = "rbxassetid://4483345998", Time = 5})
+              OrionLib:MakeNotification({
+            Name = "Thành công!",
+            Content = "Giao diện đã chạy bằng link Raw của bạn",
+            Image = "rbxassetid://4483345998",
+            Time = 5
+        })
       end    
 })
 
